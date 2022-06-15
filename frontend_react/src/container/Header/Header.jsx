7455de1526264c3f720914c9abb24d616f15a5e3
Abbas Kherani <abbaskherani1998@gmail.com>
2022-06-16 00:08:14 +0530

    new changes

--- frontend_react/src/container/Header/Header.jsx
+++ frontend_react/src/container/Header/Header.jsx
@@ -50,7 +50,7 @@ const Header = () => {
         <motion.img
           whileInView={{ scale: [0, 1] }}
           transition={{ duration: 0.5, ease: 'easeInOut' }}
-          className="overlay-circle"
+          className="overlay_circle"
           src={images.circle}
           alt='profile_circle' />
       </motion.div>
@@ -60,7 +60,7 @@ const Header = () => {
       whileInView = {scaleVariants.whileInView}
       className="app__header-circles">
 
-      {[images.flutter, images.redux, images.sass].map((circle, index) => (
+      {[images.javascript, images.redux, images.sass].map((circle, index) => (
         <div className="circle-cmp app__flex" key={`circle-${index}`}>
         <img src={circle} alt="circle" />
 
